dab521a7847b13ea073733b365c519d341ad7a62
Dmitriy Makeev <makeev095095@mail.ru>
2024-09-12 12:43:35 +0300

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Макей 😈 on 12.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
